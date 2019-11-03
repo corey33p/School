17554933674ad80b6b895aa581d37893ff4b1f39
corey33p <corey33p@gmail.com>
2019-11-03 04:57:53 -0600

    Add files via upload

--- EECS 268/LAB 09/Executive.cpp
+++ EECS 268/LAB 09/Executive.cpp
@@ -1,7 +1,14 @@
 using namespace std;
 #include <iostream>
+#include <time.h>
 #include <random>
+#include <fstream>
 #include "Executive.h"
+#include "mergeSort.cpp"
+#include "quickSort.cpp"
+#include "selectionSort.cpp"
+#include "bubbleSort.cpp"
+#include "insertionSort.cpp"
 
 Executive::Executive(std::string fileName){
     ifstream file(fileName);
@@ -17,11 +24,12 @@ Executive::Executive(std::string fileName){
     file >> state;
     std::string method;
     file >> method;
-    Executive::generate(count);
-    Executive::printList();
+    double time = Executive::sort(count,state,method);
+    cout<<"time: "<<time<<endl;
 }
 
 void Executive::generate(int n){
+    if (listExists){delete [] Executive::theList;}
     Executive::size = n;
     Executive::theList = new double[n];
     //
@@ -33,6 +41,7 @@ void Executive::generate(int n){
         double randy = unif(re);
         Executive::theList[i]=randy;
     }
+    Executive::listExists = true;
 }
 
 void Executive::printList(){
@@ -50,4 +59,98 @@ void Executive::printList(){
 
 Executive::~Executive(){
     delete [] Executive::theList;
+}
+
+// lab9 <data_size> <random|ascending|descending> <selection|insertion|bubble|merge|quick>
+double Executive::sort(int count,std::string state,std::string method){
+    cout<<"Sorting n = "<<count<<"; "<<state<<"; "<<method<<endl;
+    Executive::generate(count);
+    if (state=="robot"){ Executive::robotSort();};
+    if (state!="random"){
+        // quickSort(Executive::theList,0,count-1);
+        mergeSort(Executive::theList,count);
+        // selectionSort(Executive::theList,count);
+        // bubbleSort(Executive::theList,count);
+        // insertionSort(Executive::theList,count);
+    } 
+    if (state=="descending"){
+        Executive::reverseList(Executive::theList,0,count-1);
+    }
+    clock_t start;
+    clock_t end;
+    if (method=="selection"){
+        start = clock();
+        selectionSort(Executive::theList,count);
+        end = clock();
+    } else if (method=="insertion"){
+        start = clock();
+        insertionSort(Executive::theList,count);
+        end = clock();
+    } else if (method=="bubble"){
+        start = clock();
+        bubbleSort(Executive::theList,count);
+        end = clock();
+    } else if (method=="merge"){
+        start = clock();
+        mergeSort(Executive::theList,count);
+        end = clock();
+    } else if (method=="quick"){
+        start = clock();
+        selectionSort(Executive::theList,count);
+        end = clock();
+    }
+    double time = (end - start) / (double) CLOCKS_PER_SEC;
+    return time;
+}
+
+void Executive::reverseList(double arr[], int start, int end){
+    while (start < end) { 
+        double temp = arr[start];  
+        arr[start] = arr[end]; 
+        arr[end] = temp; 
+        start++; 
+        end--; 
+    }  
+}
+
+void Executive::robotSort(){
+    std::string methods[5] = {"selection","insertion","bubble","merge","quick"};
+    std::string states[3] = {"random","ascending","descending"};
+    int testsPer = 11;
+    // int testsPer = 6;
+    int ranges[5][2] = {{50000,500000},{50000,500000},{50000,500000},{500000,100000000},{50000,500000}};
+    // int ranges[5][2] = {{50000,100000},{50000,100000},{50000,100000},{500000,10000000},{50000,100000}};
+    std::string output = "";
+    int methodIndex = 0;
+    for (auto & method : methods) {
+        output.append(method);
+        output.append("\n");
+        for (auto & state : states) {
+            output.append(state);
+            output.append("\n");
+            int start = ranges[methodIndex][0];
+            int end = ranges[methodIndex][1];
+            double increment = (end - start) / double (testsPer-1);
+            for (int i = 0;i<testsPer;i++) {
+                int length = start + i * increment;
+                if ((method == "insertion")||(method == "bubble")){
+                    if (state == "ascending"){
+                        length = length * 100;
+                    }
+                }
+                double time = Executive::sort(length,state,method);
+                cout<<"    time: "<<time<<endl;
+                output.append(std::to_string(length));
+                output.append(", ");
+                output.append(std::to_string(time));
+                output.append("\n");
+            }
+        }
+        methodIndex++;
+        output.append("\n");
+    }
+    std::ofstream out("output.txt");
+    out << output;
+    out.close();
+    return;
 }
--- EECS 268/LAB 09/Executive.h
+++ EECS 268/LAB 09/Executive.h
@@ -13,8 +13,12 @@ class Executive{
     private:
         void generate(int n);
         double* theList;
+        bool listExists;
         int size;
         void printList();
+        void reverseList(double arr[], int start, int end);
+        double sort(int count,std::string state,std::string method);
+        void robotSort();
 };
 
 #endif
